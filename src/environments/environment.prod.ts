26c37e69f14fd70dce0b8678fed4dc6c9fdfe434
Josebratt <josebratt@gmail.com>
2022-04-29 13:27:56 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'lina-buitrago',
+    appId: '1:1007639259427:web:fb8a3058c64a54a343c496',
+    storageBucket: 'lina-buitrago.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyA-2YRZQY7XBbybo-ZaoZtxfSpNH2wUQv8',
+    authDomain: 'lina-buitrago.firebaseapp.com',
+    messagingSenderId: '1007639259427',
+    measurementId: 'G-5SQC40NTE2',
+  },
   production: true
 };
